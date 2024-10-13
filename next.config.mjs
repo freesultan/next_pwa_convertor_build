0b68f763e37339f6e1101f736bb09dd93ba394c2
freesultan <freetayyeb@gmail.com>
2024-10-13 12:51:20 +0000

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  pwa: {
+    dest: 'public', // Destination for the service worker file
+    register: true, // Automatically register the service worker
+    skipWaiting: true, // Immediately take control of the page
+    // Optionally, you can specify the `swDest` if you want to customize the service worker filename
+    // swDest: 'sw.js', // Uncomment if you want to specify the service worker file name
+  },
+  reactStrictMode: true, // Enable React's Strict Mode
+  trailingSlash: true, // Optional: adds a trailing slash to the end of URLs
+});
 
 export default nextConfig;
